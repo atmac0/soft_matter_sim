8bbbb391be27cb8bf6581982cf587a6db7b590ec
atmac0 <ilovemrknets@gmail.com>
2019-11-30 00:24:52 -0500

    drawing working

--- data_types.h
+++ data_types.h
@@ -1,9 +1,41 @@
-enum Particle_type: short {SQUARE, LONG_RECTANGLE, SHORT_RECTANGLE};
+#pragma once
 
-const int SQUARE_SIDE_LENGTH = 20;
+enum Particle_type: int16_t {SQUARE, LONG_RECTANGLE, SHORT_RECTANGLE};
 
-struct coords
+//Define the number of cells, by width and height, of the field.
+#define FIELD_WIDTH  100
+#define FIELD_HEIGHT 100
+
+#define CELL_SIZE 1 //size of each cell in the field in micro-meters. 
+
+#define SQUARE_SIDE_LENGTH 20 //please keep this number even. This number is the length of one side of the square in micrometers.
+#define NUM_SIDES_SQUARE 4
+
+struct coord_t
 {
-  int x;
-  int y;
+  int32_t x;
+  int32_t y;
 };
+
+
+/*********************************/
+/* struct png_header		 */
+/* {				 */
+/*   int length;		 */
+/*   int type;			 */
+/*   int data;			 */
+/*   int CRC;			 */
+/* };				 */
+/* 				 */
+/* const struct png_signature	 */
+/* {				 */
+/*   char transmission = 137;	 */
+/*   char p            = 80;	 */
+/*   char n            = 78;	 */
+/*   char g            = 71;	 */
+/*   char cr           = 13;	 */
+/*   char lf           = 10;	 */
+/*   char cz           = 26;	 */
+/*   char lf2          = 10;     */
+/* }				 */
+/*********************************/
